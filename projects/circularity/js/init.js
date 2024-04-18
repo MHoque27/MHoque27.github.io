c7329964e791c87da050848dfc7a20d91a1138e3
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2024-04-18 00:39:34 +0000

    Finished Circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,11 +32,9 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle()
-        drawCircle()
-        drawCircle()
-        drawCircle()
-        drawCircle()
+        for (let i = 0; i < 100; i++) {
+            drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -49,21 +47,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
-            
+            //deleted because loop
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
+            //deleted because loop
 
             // TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+            }
             
         }
     
@@ -78,6 +70,19 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
+            // if circ is past bottom side
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
+            // if circ gone past left side
+            if ( circle.x < 0) {
+                circle.x = canvas.width;
+            }
+            // if circle is past top side
+            if ( circle.y < 0) {
+                circle.y = canvas.height;
+            }
+            
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
