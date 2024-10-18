14569a23cce38ff2034c4fae36ddfa63d9126ba8
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2024-10-18 00:55:34 +0000

    i got up to todo 5 and uhh stopped midway through

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,23 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,  
+  };
   // Game Item Objects
-
+  var walker = {
+    positionX: 0,
+    positionY:0,
+    speedX: 0,
+    speedY: 0,
+  }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +44,20 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.LEFT) {
+      console.log("left pressed");
+    }
+    if (event.which === KEY.UP) {
+      console.log("up pressed");
+    }
+    if (event.which === KEY.RIGHT) {
+      console.log("right pressed");
+    }
+    if (event.which === KEY.DOWN) {
+      console.log("down pressed");
+    }
+    
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -49,6 +71,13 @@ function runProgram(){
 
     // turn off event handlers
     $(document).off();
+  }
+  function repositionGameItem() {
+    positionX += speedX;
+    positionY += speedY;
+  }
+  function redrawGameItem() {
+    $("#walker").css("left", positionX);
   }
   
 }
