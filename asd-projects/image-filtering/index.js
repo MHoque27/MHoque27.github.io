2062d4fb6df541f309a1002a990395816ec62eb8
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2024-11-15 01:42:05 +0000

    finished todo 7, finished image filtering yayys

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
+  applyFilterNoBackground(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,18 +33,50 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction) {
+  for (let i = 0; i < image.length; i++) {
+    for (let j = 0; j < image[i].length; j++) {
+      var rgbString = image[i][j]
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers)
+      image[i][j] = rgbString
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[0][0];
+  for (let i = 0; i < image.length; i++) {
+    for (let j = 0; j < image[i].length; j++) {
+      if (image[i][j] != backgroundColor) {
+        var rgbString = image[i][j]
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers)
+        image[i][j] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(num1) {
+  return Math.max(0, Math.min(255, num1));
+}
 
 // TODO 3: Create reddify function
-
+function reddify(arrayOne) {
+  arrayOne[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
-
+function decreaseBlue(arrayTwo) {
+  arrayTwo[BLUE] = keepInBounds(arrayTwo[BLUE] - 50);
+}
+function increaseGreenByBlue(arrayThree) {
+  arrayThree[GREEN] = keepInBounds(arrayThree[GREEN] + arrayThree[BLUE]);
+}
 
 // CHALLENGE code goes below here
