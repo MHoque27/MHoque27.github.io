c033b576295e29352bf8d826a5b56c5eab5420bc
IoT <IoT@macmini.lan>
2025-02-18 19:50:21 -0600

    i finished and am working on todo 5

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -1,11 +1,12 @@
 // fetchServer.js file
 const http = require("http");
 const port = 1337;
-
+var args = process.argv.slice(2);
 http
     .createServer(async function (req, res) {
+        var url = args[0] ? args[0] : "<a default url>";
         res.writeHead(200, { "Content-Type": "text/html" });
-        var fetchResponse = await fetch("https://MHoque27.github.io");
+        var fetchResponse = await fetch(url);
         if (fetchResponse.ok == true) {
             html = await fetchResponse.text();
             res.write(html);
