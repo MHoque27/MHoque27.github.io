98f637b93a84b068723e435b0b311ac84066a2d6
IoT <IoT@macmini.lan>
2025-02-25 19:43:12 -0600

    finished fetch server and race server

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -4,8 +4,14 @@ const port = 1337;
 var args = process.argv.slice(2);
 http
     .createServer(async function (req, res) {
+        console.log(args);
         var url = args[0] ? args[0] : "<a default url>";
-        res.writeHead(200, { "Content-Type": "text/html" });
+        if (args[1] == "text/html") {
+            res.writeHead(200, { "Content-Type": "text/html" });
+        } else if (args[1] == "text/plain") {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+        }
+
         var fetchResponse = await fetch(url);
         if (fetchResponse.ok == true) {
             html = await fetchResponse.text();
--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const async = require("async");
-
+let d = new Date();
+let startTime = d.getTime();
 const port = 8686;
 
 http.createServer(function(req, res) {
@@ -9,17 +10,38 @@ http.createServer(function(req, res) {
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 7: Get the start time for the race
-    
-
+    let d = new Date();
+    let startTime = d.getTime();
     // TODO 12: Make the whole thing parallel
-    async.series( 
+    async.parallel( 
         // TODO 9: Supply an array of functions
         [
+            function (callback) {
+                wrapper(callback);
+            },
+            function (callback) {
+                wrapper(callback);
+            },
+            function (callback) {
+                wrapper(callback);
+            },
+            function (callback) {
+                wrapper(callback);
+            },
             
         ],
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
-
+            res.write("Results:\n");
+            var victoryOrder = sortTogether(racers, results);
+            for (let i = 0; i < victoryOrder.length; i++) {
+                res.write(victoryOrder[i] + "\n");
+            }
+            let d = new Date();
+            let endTime = d.getTime();
+            let duration = endTime - startTime;
+            res.write("Race Duration: " + duration + "ms\n");
+            res.end();
         }
     );
     
@@ -27,7 +49,9 @@ http.createServer(function(req, res) {
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
-
+    setTimeout(function () {
+        callback(null, d.getTime())
+    }, Math.random() * 1000);
 }
 
 // sortTogether takes in an array of racer names and an array of times that the racers finished the race.
