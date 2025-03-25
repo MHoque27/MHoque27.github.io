92e439ea6d2dd87ba002816dbe9561921212ef7a
mhoque27 <mhoque27@bfhsla.org>
2025-03-25 18:38:11 -0500

    finished catch Server

--- iot-projects/catch-server/safeServer.js
+++ iot-projects/catch-server/safeServer.js
@@ -6,12 +6,25 @@ function requestFunction(req, res) {
     try {
         if (req.method === "GET") {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.write(serverStatus);
+            res.write(serverStatus.message);
             return;
         } else if (req.method === "DELETE") {
 
         } else if (req.method === "PUT") {
-        
+            var body = "";
+            req.on('data', (stream) => {
+                            console.log({stream})
+
+                body += stream.toString();
+            });
+            req.on('end', (stream) => {
+                serverStatus = {};
+                console.log(body)
+                serverStatus = JSON.parse(body);
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+            });
+                            res.write("The server has been updated.");
+
         } else if (req.method === "POST") {
         
         } else {
