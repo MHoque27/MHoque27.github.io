18b02fbfc935e48b4849ccb243ef1c4c1fd00a27
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2025-09-17 00:46:05 +0000

    Wrote down code but does not work

--- vgp-projects/init-createjs/index.js
+++ vgp-projects/init-createjs/index.js
@@ -4,10 +4,11 @@
  */
 (function(window, createjs) {
   // TODO 5: Initialize CreateJS //
-  
+  const canvas = document.getElementById("canvas");
+  const stage = new createjs.Stage(canvas);
 
   // TODO 6: Set the framerate of the Ticker
-  
+  createjs.Ticker.framerate = 60;
 
 
   /*
@@ -21,30 +22,45 @@
 
     
   // CREATE A BACKGROUND //
-
+  const bg = new createjs.Shape();
+  console.log(bg);
+  bg.graphics.beginFill("rgba(255, 116, 116, 1)").drawCricle(350, 350, 375);
     
   // CREATE A CIRCLE //
+  const eyeContainer = new createjs.Container();
 
+  const leftEye = new createjs.Shape();
+  const rightEye = new createjs.Shape();
   
+  leftEye.graphics.beginFill("blue").drawRect(20, 20, 60, 10);
 
+  rightEye.graphics.beginFill("#FFBF00").drawCircle(150, 20, 25);
 
   // ADD DISPLAY OBJECTS TO STAGE //
+  eyeContainer.addChild(leftEye, rightEye);
+  stage.addChild(bg, eyeContainer);
 
 
+  stage.update();
 
   // TODO 8: Listen to the 'tick' event  //
-  
+  let tickHandler = createjs.Ticker.on("tick", onTick); // action/event, eventHandler/callback function
   
 
   // TODO 9: Handle the 'tick' event //
-  
+  function onTick(event) {
+    update(event);
+  }
   
 
   /*
    * TODO 10: Implement an update Function, after making 
    * changes to assets, it must call stage.update(); 
    */
-  
+  function update(event) {
+    leftEye.x++;
+    rightEye.x--
+  }
   
 
 }(window, window.createjs));
