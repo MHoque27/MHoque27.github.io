90bd0393a38b7474cf92da387f10c8188d77c43e
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2025-11-05 01:38:46 +0000

    finished calculate rotation

--- vgp-projects/calculate-rotation/index.js
+++ vgp-projects/calculate-rotation/index.js
@@ -1,5 +1,5 @@
 // TODO 4: add a param for your game lib last //
-(function(window, opspark) {
+(function(window, opspark, finiteArtillery) {
   console.log('index.js initialized!');
 
   const
@@ -17,9 +17,12 @@
   
   // TODO 5: Center the ship on the stage //
   
+  ship.x = canvas.width / 2
+  ship.y = canvas.height / 2
+  console.log(ship)
 
   // TODO 6: Add the ship to the stage //
-  
+  stage.addChild(ship)
 
   
   function update(event) {
@@ -34,10 +37,13 @@
      * method takes two points. What do you need to do to translate
      * these values such that they're packed into a point?
      */
-    
-    
+    var ting = {
+      x: stage.mouseX,
+      y: stage.mouseY,
+    }
+    const degrees = finiteArtillery.numz.getAngleDegrees(ship, ting)
     // TODO 8: Set the ship's rotation property to the degrees //
-    
+    ship.rotation = degrees
     
     
     /*
@@ -45,7 +51,7 @@
      * with the current angle degrees. Degrees will be a value 
      * between π and -π, or, 180 and -180.
      */
-    // assets.updateText(textfield, `Degrees: ${degrees.toFixed(3)}°`, canvas);
+    assets.updateText(textfield, `Degrees: ${degrees.toFixed(3)}°`, canvas);
   }
 
   engine
@@ -53,4 +59,4 @@
     .activateTick();
 
 // TODO 3: pass your game lib last with, window.my-game-lib //
-}(window, window.opspark));
+}(window, window.opspark, window.finiteArtillery));
