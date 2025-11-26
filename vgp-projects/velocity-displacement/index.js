ccdb0115328928a26f48a38de22d2d7334718719
MHoque27 <159090598+MHoque27@users.noreply.github.com>
2025-11-26 00:02:48 +0000

    finished velocity-displacement

--- vgp-projects/velocity-displacement/index.js
+++ vgp-projects/velocity-displacement/index.js
@@ -1,5 +1,5 @@
 // TODO 4: Change *my-game-lib* to the name of your game lib
-(function(window, opspark, *my-game-lib*) {
+(function(window, opspark, finiteArtillery) {
   const
     engine = opspark.V6().activateResize(),
     canvas = engine.getCanvas(),
@@ -34,7 +34,7 @@
      * is available to you in this scope as, "this".
      * 2. What are the x and y forces acting on our ship?
      */
-    
+    finiteArtillery.phyz.updateVelocity(this, this.propulsion, this.propulsion);
     
     
     // also check if the ship needs to rebound off a boundary //
@@ -76,6 +76,15 @@
   // listen for user releasing keys //
   document.onkeyup = function(event) {
     // TODO 13: How do we stop the application of forces?
+    if (event.key === 'ArrowUp') {
+      ship.propulsion = 0;
+    }
+    if (event.key === 'ArrowRight') {
+      ship.rotationalVelocity = 0;
+    }
+    if (event.key === 'ArrowLeft') {
+      ship.rotationalVelocity = 0;
+    }
     
   };
   
@@ -90,25 +99,29 @@
     // check for hit on either side of area //
     if (body.x + radius > right) {
       // we've struck the right side of the area //
-      body.x = right - radius;
+      //maybe include vvvv
+      // body.x = right - radius;
       body.velocityX *= -1;
-    } else if ( /* TODO 9: Check if body's hit left side */ false ) {
+    } else if (body.x - radius < left) {
       // we've struck the left side of the area //
       // TODO 10: Code the reaction to hitting the left side
-      
+      //maybe include vvvv
+      // body.x = left + radius;
+      body.velocityX *= -1;
     }
 
     // check for hit on top or bottom //
     if (body.y - radius < top) {
       // we've struck the right side of the area //
-      body.y = top + radius;
+      // body.y = top + radius;
       body.velocityY *= -1;
-    } else if ( /* TODO 11: Check if body's hit bottom */ false ) {
+    } else if (body.y + radius > bottom) {
       // we've struck the bottom of the area //
       // TODO 12: Code the reaction to hitting the bottom
-      
+      // body.y = top - radius;
+      body.velocityY *= -1
     }
   }
   
   // TODO 3: replace *my-game-lib* with the name of your game lib //
-}(window, window.opspark, window.*my-game-lib*));
+}(window, window.opspark, window.finiteArtillery));
